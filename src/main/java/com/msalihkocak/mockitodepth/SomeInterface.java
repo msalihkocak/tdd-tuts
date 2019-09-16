41ed1c76f5a85ef6805822bec12d1f27f03658b2
Mehmet Salih Koçak <msalihkoc@MSalih.Koc>
2019-09-16 17:49:35 +0300

    Add more mockito examples

---
+++ src/main/java/com/msalihkocak/mockitodepth/SomeInterface.java
@@ -0,0 +1,12 @@
+package com.msalihkocak.mockitodepth;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface SomeInterface {
+    int getMatchingSize(int x, int y);
+
+    void receiveList(List<String> list);
+
+    boolean isFileValid(String filename) throws IOException;
+}
